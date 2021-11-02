c868705990362d4a6fde1f6aa88897c7d7acaab8
unknown <towhid2302@gmail.com>
2021-11-02 19:55:58 +0545

    item add and remove functionality added as well as awesome alert function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,58 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg: '', type: ''})
+
+  const  handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!name){
+      showAlert(true,'Please enter value','danger')
+    }
+    else if(name && isEditing){
+      //deal with editing
+    }
+    else{
+      //show alert
+      showAlert(true,'Item added successfully','success')
+      const newitem = {id: new Date().getTime().toString(),title: name }
+      setList([...list,newitem]);
+      setName('');
+    }
+  }
+
+  const showAlert = (show="false", msg="", type="") =>{
+      setAlert({show,msg,type})
+  }
+  const clearList =()=>{
+    showAlert(true,'Items removed successfully','danger')
+    setList([]);
+  }
+
+
+  return <section className="section-center">
+
+    <form className="grocery-form" onSubmit={handleSubmit} >
+      {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>   }
+      <h3>Grocery Bud</h3>
+      <div className="form-control">
+        <input className="grocery" value={name} onChange={(e) => setName(e.target.value)} placeholder="item name" type="text" name="" id="" />
+        <button type="submit" className="submit-btn">
+          {isEditing? 'Edit' : 'Add'  }
+        </button>
+      </div>
+    </form>
+
+    <div className="grocery-container">
+      <List items={list} />
+      <button onClick={clearList} className="clear-btn">
+        clear all items
+      </button>
+    </div>
+  </section>
 }
 
 export default App
--- src/List.js
+++ src/List.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({items}) => {
+  
+  return <div className="grocery-list" >
+    {
+      items.map((item) => {
+        const {title,id} = item;
+        return  <article className="grocery-item" key="id" >
+            <p className="title">
+              {title} {id}
+            </p>
+            <div className="btn-container">
+              <button className="edit-btn">
+                <FaEdit />
+              </button>
+              <button className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+        </article>
+      })
+    }
+  </div>
 }
 
 export default List
